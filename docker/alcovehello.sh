cb327d50b7114cfeeae7115f45f3a579bbf1ff24
bioneos <bioneos@wireless-nat-guest-inside.uiowa.edu>
2023-07-12 10:52:44 -0500

    Added an alpine base-image that generates a SSH key-pair, along with two containers using that image

---
+++ docker/alcovehello.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#Makes it so that the script will immediately exit if a command fails
+set -e
+
+# Run SSH daemon in the foreground. Starts the SSH server process and keeps it active.
+#
+# Sources where this is used:
+# https://forums.docker.com/t/how-to-run-a-service-in-an-image/68224
+# https://dev.to/s1ntaxe770r/how-to-setup-ssh-within-a-docker-container-i5i
+#/usr/sbin/sshd -D
+
--- docker/helloworld.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true; do
-    echo "Hello, World!"
-    sleep 60
-done
