31eabfdc9718cf89087e086cd8aec1b73a45460b
Rith Sivaprakash <rithsiva22@gmail.com>
2023-07-13 14:36:11 -0500

    config files are auto-generated

--- docker/alcovedev.sh
+++ docker/alcovedev.sh
@@ -2,6 +2,68 @@
 
 #Makes it so that the script will immediately exit if a command fails
 set -e
+mkdir -p config
+mkdir -p config/machines
+
+content_alcove="
+                log_level='DEBUG'
+
+                data_dir='data/'
+
+                [secure]
+                key='./etc/alcove/ssl/ssl.key'
+                cert='./etc/alcove/ssl/ssl.crt'
+
+                [rsync]
+
+                [rsync.retry]
+
+                [notifications]
+
+                summary_schedule='0,1,2,3,4,5,6;[17:01]'
+
+
+                email_to[]='your.email@gmail.com'
+
+                email_from='your.email@gmail.com'
+
+
+                [notifications.smtp]
+
+                host='smtp.gmail.com'
+
+
+                port=587
+
+
+                user='your.email@gmail.com'
+
+
+                pass='your_password'
+
+                [notifications.sms]
+
+
+                [notifications.slack]"
+
+file_path_alcove="config/alcove.ini"
+echo "$content_alcove" > "$file_path_alcove"
+content="
+         name=alpha
+
+         host=localhost
+
+         backup_directories[] = /backup-test
+
+         ignore_extensions[] = .testignored
+
+         ignore_files[] = /backup-test/ignored
+         ignore_files[] = /backup-test/ignore-me.txt"
+
+# File path
+file_path="config/machines/machine.ini"
+
+echo "$content" > "$file_path"
 
 # Run SSH daemon in the foreground. Starts the SSH server process and keeps it active.
 #
